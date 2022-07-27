eff465cc24b1880f624cb33bdfd3b400a8ef2142
Vincenzo Esposito <aenon.esposito@gmail.com>
2022-07-27 17:29:47 +0200

    make PJSIP_MAX_URL_SIZE change in config-site.h - #48

--- patches/fixed_callid/patch.sh
+++ patches/fixed_callid/patch.sh
@@ -16,7 +16,4 @@ cd "$PJPROJECT_BASE_FOLDER"
 patch -p0 < callid.patch
 rm callid.patch
 
-# Changing PJSIP_MAX_URL_SIZE --- see README.md for more information
-sed -i -r 's~(#[ \t]*(define[ \t]+PJSIP_MAX_URL_SIZE)[ \t]+)(256){1}~\1512~g' $PJPROJECT_BASE_FOLDER/pjsip/include/pjsip/sip_config.h
-
 cd "$CURDIR"
